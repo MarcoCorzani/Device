61402d961e883751d2afd2d6d80accb9bf5cbf33
Marco <Marco@Marcos-iMac.fritz.box>
2015-01-07 20:32:32 +0100

    Anfang

--- Device/ViewController.swift
+++ Device/ViewController.swift
@@ -10,10 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var label: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+    if UIDevice.currentDevice().userInterfaceIdiom == .Pad {
+        
+            self.label.text = "Das ist ein IPad"
+    } else {
+        
+        var detectionSize:CGFloat = 0
+        
+        if self.interfaceOrientation == .Portrait{
+            detectionSize = self.view.bounds.height
+        }else {
+            detectionSize = self.view.bounds.width
+        }
+        
+        
+        // Gerät nach der Höhe ermitteln
+        //switch self.view.bounds.height {
+        
+        //So egal wie rum es gehalten wird!
+        switch detectionSize {
+        
+        case 480:
+            self.label.text = "Das ist ein IPhone 4"
+        case 568:
+            self.label.text = "Das ist ein IPhone 5"
+        case 667:
+            self.label.text = "Das ist ein IPhone 6"
+        case 736:
+            self.label.text = "Das ist ein IPhone 6 Plus"
+            
+        default:
+            self.label.text = "Das ist was ganz anderes"
+        }
+        
+      }
+        
     }
+    
+        
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
